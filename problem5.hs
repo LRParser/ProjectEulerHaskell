594fbf0480ff442c29b04cac557430ee9bcddcda
LRParser <lrparser@server.fake>
2013-06-26 21:13:19 -0400

    Problem 5 answer

--- problem5.hs
+++ problem5.hs
@@ -1,6 +1,12 @@
 -- Smallest # divisble by 1 thru 20
 
+-- Native solution
 divisors = [1..20]
-allNums = [1..3]
-isDivisible x y = (x `mod` y) == 0 && (x `div` y) == 1
+allNums = [1..]
+isDivisible x y = (x `mod` y) == 0
 allDivisible x = not (elem False (map (isDivisible x) divisors))
+firstDivisible = head (filter allDivisible allNums)
+
+-- Quick solution
+
+ans = foldr1 (*) divisors
